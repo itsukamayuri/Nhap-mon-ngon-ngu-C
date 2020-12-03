55591bbedd610026957a776426f563aee18d62ea
itsukamayuri <72533420+itsukamayuri@users.noreply.github.com>
2020-12-03 08:25:51 +0700

    Add files via upload

---
+++ Bài tập 1/baitap.c
@@ -0,0 +1,17 @@
+#include	<stdio.h>
+
+int main()
+{
+	//khai bao bien DT,CV va R kieu double
+	double  DT,CV,R;
+	//khai bao ten PI kieu double
+	double PI=3.14;
+	//tinh chu vi ,dien tich hinh  tron
+	  R=1.5;
+     
+    CV =2*R*PI;
+    DT =R*R*PI;
+    printf ( "CV hinh tron la: %0.1f\n", CV);
+    printf ("DT hinh tron la:%0.1f" , DT);
+	return 0;		
+}
