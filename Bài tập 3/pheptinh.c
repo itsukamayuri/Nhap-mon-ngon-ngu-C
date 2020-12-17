077e2d9a350bca0168f2a5063835bc42e0e77456
bảo <72533420+itsukamayuri@users.noreply.github.com>
2020-12-17 11:01:08 +0700

    Add files via upload

---
+++ Bài tập 3/pheptinh.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	char pt;
+int a = 5,b=3;
+printf(" Nhap phep tinh: ");
+scanf("%c",&pt);
+switch(pt){
+  case'+':
+  printf("a + b =");break;
+  case '-':
+  printf("a - b = ");break;
+  default:
+  printf("vui long nhap phep tinh");
+}
+return 0;}
+ 
+
+  
