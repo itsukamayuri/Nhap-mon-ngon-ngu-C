f2a591bb114dbe2cc6d7a9b039cc44417d163bb5
bảo <72533420+itsukamayuri@users.noreply.github.com>
2020-12-17 09:13:13 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -6,18 +6,18 @@ int main(void){
   //nhap gia tri//
   scanf("%d",&a);
   //khai bao don gia //
-   printf("nhap don gia");
-    //nhap gia tri b//
-    scanf("%d",&b);
-    //khai bao bien//
-    int soluong, dongia, thanhtien;
-    //cong thuc tinh//
-    thanhtien = a * b;
-    //neu so luong lon hon5 va don gia lon hon 500//
-    if(a>=5){thanhtien=(a*b)*0.5;
-     a=5;}
-     if(b>=500){
-    b=500;
-    printf("thanhtien=%d",thanhtien);}
+  printf("nhap don gia");
+  //nhap gia tri b//
+  scanf("%d",&b);
+  //khai bao bien//
+  int soluong, dongia, thanhtien;
+  //cong thuc tinh//
+  thanhtien = a * b;
+  //neu so luong lon hon5 va don gia lon hon 500//
+  if(a>=5){thanhtien=(a*b)*0.5;
+  a=5;}
+  if(b>=500){
+  b=500;
+  printf("thanhtien=%d",thanhtien);}
   
   return 0;}
