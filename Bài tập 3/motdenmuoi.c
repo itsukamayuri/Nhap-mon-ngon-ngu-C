fb87837eb46d8557fc472c579b36d5796f6ebbc9
bảo <72533420+itsukamayuri@users.noreply.github.com>
2020-12-24 09:36:09 +0700

    Add files via upload

---
+++ Bài tập 3/motdenmuoi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+	//khai bao bien
+int i;
+ //khai bao vong lap
+ for  (i = 1; i < 11 ; ++i)
+ //hien thi ra man hinh
+ printf("%d  ",i);
+ //khai bao vong lap
+ for  (i = 1; i < 11 ; ++i)
+ //hien thi ra man hinh
+ printf("%d\n", i);
+ 
+ 
+ 
+
+  return 0;
+}
+
