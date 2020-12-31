d0a06577eb2f12a383b0ec5a412c683f3c5ebd18
bảo <72533420+itsukamayuri@users.noreply.github.com>
2020-12-31 11:04:37 +0700

    Update review0_2.c

--- Bài tập 3/review0_2.c
+++ Bài tập 3/review0_2.c
@@ -8,16 +8,17 @@ scanf("%d\n%d", &a, &b);
 char *str[] = {"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 //khai bao vong lap
 for(i=a;i<=b;i++)
-{
+{//cac truong hop
 if(i>9)
 {
 if(i%2==0)
+// hien thi chang le
 printf("even\n");
 else
 printf("odd\n");
 }
 else
-{
+{// hien thi chuoi string
 printf("%s\n",str[i]);
 }
 }
