4529c2e79993e548c3b0fd997b66a76e10b5e5bd
bảo <72533420+itsukamayuri@users.noreply.github.com>
2020-12-31 11:03:08 +0700

    Add files via upload

---
+++ Bài tập 3/review0_3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//khai bao bien
+int main() {
+  //khai bao bien a
+  int a;
+  // khai bao vong lap
+  for (a=1 ;a <=20;a=a+2){
+  // cac truong hop
+  if (a!=5)
+  if (a!=11)
+  if  (a!=15)
+  // hien thi ra man hinh
+  printf("%d\n",a);
+  }
+
+
+   return 0;
+}
