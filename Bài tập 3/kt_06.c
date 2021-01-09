0d85714a7b3ce5ac0243f22bf26fb1afb62f29e5
bảo <72533420+itsukamayuri@users.noreply.github.com>
2021-01-09 10:36:06 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -2,24 +2,24 @@
 int main(void){
   //h=4:4hang
   //hang 1:1ngoi sao 6 khoang trang
-  //hang 2:3ngoi sao 2khoang trang
-  //hang 3:5ngoi sao 1khoang trang
+  //hang 2:3ngoi sao 4khoang trang
+  //hang 3:5ngoi sao 2khoang trang
   //hang 4:7ngoi sao 0 khoang trang
 
   //h=3:3hang
   //hang 1:1ngoi sao 6 khoang trang
-  //hang 2:3ngoi sao 2khoang trang
+  //hang 2:3ngoi sao 4khoang trang
   //hang 3:5ngoi sao 1khoang trang
   //hang 4:
 
   //h2=:2hang
-  //hang 1: 1ngoi sao 6 khoang trang
-  //hang 2:3ngoi sao 2khoang trang
+  //hang 1:1ngoi sao 6 khoang trang
+  //hang 2:3ngoi sao  4khoang trang
   //hang 3:
   //hang 4:
 
   //h=1:1hang
-  //hang 1: 1ngoi sao 6 khoang trang
+  //hang 1:1ngoi sao 6 khoang trang
   //hang 2:
   //hang 3:
   //hang 4:
