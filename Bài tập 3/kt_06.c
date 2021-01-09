0b744df310f3640799f41801cf31612d0d362318
bảo <72533420+itsukamayuri@users.noreply.github.com>
2021-01-09 10:34:30 +0700

    Update and rename kt_03.c to kt_06.c

---
+++ Bài tập 3/kt_06.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(void){
+  //h=4:4hang
+  //hang 1:1ngoi sao 6 khoang trang
+  //hang 2:3ngoi sao 2khoang trang
+  //hang 3:5ngoi sao 1khoang trang
+  //hang 4:7ngoi sao 0 khoang trang
+
+  //h=3:3hang
+  //hang 1:1ngoi sao 6 khoang trang
+  //hang 2:3ngoi sao 2khoang trang
+  //hang 3:5ngoi sao 1khoang trang
+  //hang 4:
+
+  //h2=:2hang
+  //hang 1: 1ngoi sao 6 khoang trang
+  //hang 2:3ngoi sao 2khoang trang
+  //hang 3:
+  //hang 4:
+
+  //h=1:1hang
+  //hang 1: 1ngoi sao 6 khoang trang
+  //hang 2:
+  //hang 3:
+  //hang 4:
+  
+  
+  return 0;
+}
