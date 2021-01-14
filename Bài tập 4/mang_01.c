23518b54300b5a504a373e0be7b2a8fb97d20eaa
bảo <72533420+itsukamayuri@users.noreply.github.com>
2021-01-14 09:01:21 +0700

    Add files via upload

---
+++ Bài tập 4/mang_01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	int i=0;
+	//khai bao mang
+int marks[4]={3,5,7,2};
+// thuc hien vong lap
+for(i=0;i<4;i++)
+{//hien thi mang
+	printf("%d\n",marks[i]);
+}
+int tong;
+// cong thuc
+tong=3+5+7+2;
+// hien thi ket qua ra man hinh
+printf("tong=%d",tong);
+return 0;
+}
