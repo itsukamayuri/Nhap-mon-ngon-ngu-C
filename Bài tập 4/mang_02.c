6ce6113093b90b383ac0c66fca92e0d4a2c0dfe3
bảo <72533420+itsukamayuri@users.noreply.github.com>
2021-01-21 09:19:03 +0700

    Update mang_02.c

--- Bài tập 4/mang_02.c
+++ Bài tập 4/mang_02.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 int main(){
+//khai bao bien
   int n,i;
   int a[100];
+//nhap phan tu
   printf("nhap so phan tu a:\n");
   scanf("%d",&n);
+//vong lap
   for (i=0; i<n ;i++){
      printf("nhap phan tu n :\n");
    scanf("%d",&a[i]);
   }
+//khai bao bien tong
   int tong=0;
+// khai bao vong lap
   for (i=0;i<n;i++){
+//cong thuc
     tong=tong+a[i];
-    printf("tong = %d\n",tong);
-  }
-  
+//hien thi ket qua
+    printf("tong = %d\n",tong);}
 return 0;
 }
-
