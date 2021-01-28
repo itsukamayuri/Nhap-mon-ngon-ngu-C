e505cbe18905ce1f3a888086a335f48ea8818085
bảo <72533420+itsukamayuri@users.noreply.github.com>
2021-01-28 15:16:43 +0700

    Create TH_02.c

---
+++ Bài tập 5/TH_02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){//khai bao bien
+  int n,s;
+  s = 0;
+  printf("Nhap n = ");
+  scanf("%d",&n);
+  for(int i = 1; i <= n; i++)
+  s = s + i;
+  printf("\nS = 1 + 2 +...+  = %d ",s);
+  return 0; 
+}
